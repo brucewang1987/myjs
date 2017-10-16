11bd7e0bfd3228fcf546b7bb8f941bdb28132d00
brucewang1987 <30851957+brucewang1987@users.noreply.github.com>
2017-10-16 10:52:54 +0800

    Update RFinfootherbusinessJScript.js

--- RFinfootherbusinessJScript.js
+++ RFinfootherbusinessJScript.js
@@ -87,3 +87,20 @@ function QueryGateIN() {
 
 
 }
+
+
+ function exportexcel() {
+        $("#BJInfotable").table2excel({
+            exclude: ".noExl",
+            name: "Excel Document Name",
+            filename: "myFileName",
+            exclude_img: true,
+            exclude_links: true,
+            exclude_inputs: true
+        });
+    }
+
+
+
+
+
