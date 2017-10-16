ce699de944c4b465f46d83d619634bb3a9d5027b
brucewang1987 <30851957+brucewang1987@users.noreply.github.com>
2017-10-16 11:22:09 +0800

    Update RFjianhuJScript2.js

--- RFjianhuJScript2.js
+++ RFjianhuJScript2.js
@@ -286,3 +286,17 @@ function RFitemupdate() {
 
 
 }
+
+
+ function exportexcel() {
+        $("#RFInfotable").table2excel({
+            exclude: ".noExl",
+            name: "Excel Document Name",
+            filename: "myFileName",
+            exclude_img: true,
+            exclude_links: true,
+            exclude_inputs: true
+        });
+    }
+
+
