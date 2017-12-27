45ce0e8516e261175c684c505a6ca7d188240562
brucewang1987 <30851957+brucewang1987@users.noreply.github.com>
2017-12-27 11:02:11 +0800

    Update uploadfile_dwsb.js

--- uploadfile_dwsb.js
+++ uploadfile_dwsb.js
@@ -1,36 +1,45 @@
-$(function () {
-            $("#form1").submit(function () {
+ $(function () {
+                $("#form1").submit(function () {
 
-            });
+                });
 
-            $("#btnUpload").click(function () {
-                
-                //构建新的表单提交数据（只提交文本域）
-                //                var from = $("<form id='formUpload' enctype='multipart/form-data'></form>");
-                //                from.append($("#FileCtrl").clone());
-                //$(#form1).ajaxSubmit
-                $('#formUpload').ajaxSubmit({
-                    //提交前的回调函数
-                    beforeSubmit: function () {
-                        $("#btnUpload").after("<img src='images/loading.gif'  id='loading'/>");
-                    },
-                    //提交后的回调函数
-                    success: function (data) {
-                        if (data.status == "ok") {
-                            $("#result").html(data.fileName);
-                            $("#FileCtrl").val('');
-                            alert("上传成功");
-                        }
-                        else if (data.status == "error") {
-                            alert(data.msg);
-                        }
-                        $("#loading").remove();
-                        //alert(data.url);
-                    },
-                    error: function (error) { console.log(JSON.stringify(error)); },
-                    url: './FileUploadCtrl.ashx', /*设置post提交到的页面*/
-                    type: "post", /*设置表单以post方法提交*/
-                    dataType: "json" /*设置返回值类型为文本*/
+                $("#btnUpload").click(function () {
+
+                    var photoName = prompt("请输入照片名", "dwsbsbsbsb");
+
+                    /*
+                    if (photoName == "dwsb") {
+                        alert("没错，东华是刚软公司");
+
+                    }
+
+                    */
+                    //构建新的表单提交数据（只提交文本域）
+                    //                var from = $("<form id='formUpload' enctype='multipart/form-data'></form>");
+                    //                from.append($("#FileCtrl").clone());
+                    //$(#form1).ajaxSubmit
+                    $('#formUpload').ajaxSubmit({
+                        //提交前的回调函数
+                        beforeSubmit: function () {
+                            $("#btnUpload").after("<img src='images/loading.gif'  id='loading'/>");
+                        },
+                        //提交后的回调函数
+                        success: function (data) {
+                            if (data.status == "ok") {
+                                $("#result").html(data.fileName);
+                                $("#FileCtrl").val('');
+                                alert("上传成功");
+                            }
+                            else if (data.status == "error") {
+                                alert(data.msg);
+                            }
+                            $("#loading").remove();
+                            //alert(data.url);
+                        },
+                        error: function (error) { console.log(JSON.stringify(error)); },
+                        url: './FileUploadCtrl.ashx?val1=' + photoName, /*设置post提交到的页面*/
+                        type: "post", /*设置表单以post方法提交*/
+                        dataType: "json" /*设置返回值类型为文本*/
+                    });
                 });
             });
-        });
