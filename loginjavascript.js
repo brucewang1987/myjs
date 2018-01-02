815e0cd762d36c20f7d3d54461bcf18de371e462
brucewang1987 <30851957+brucewang1987@users.noreply.github.com>
2018-01-02 11:56:21 +0800

    Create loginjavascript.js

---
+++ loginjavascript.js
@@ -0,0 +1,36 @@
+ function LogIn() {
+
+            var name = $("#username").val();
+
+            if (name == "") {
+
+                alert("请输入用户名");
+                return false;
+            
+            }
+
+            
+            var pass = $("#pass").val();
+            if (pass == "") {
+
+                alert("请输入密码");
+                return false;
+
+            }
+
+            if (name == "001" && pass == "123456") {
+
+                window.location.href = "Default.aspx?backurl=" + window.location.href;
+
+            }
+            else {
+
+                alert("用户名或密码不正确");
+                window.location.reload();
+            }
+
+
+        
+        }
+
+       
