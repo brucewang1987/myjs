d8467365ddb6af62e204725a127f5e43c47db363
brucewang1987 <30851957+brucewang1987@users.noreply.github.com>
2018-01-10 08:52:44 +0800

    Update ReSetData.js

--- ReSetData.js
+++ ReSetData.js
@@ -20,7 +20,7 @@
 
                  alert(str);
 
-                 window.location.href = "RFjianhuItem.aspx?backurl=" + window.location.href;
+                // window.location.href = "RFjianhuItem.aspx?backurl=" + window.location.href;
                  
              },
              error: function (XmlHttpRequest, textStatus, errorThrown) {
