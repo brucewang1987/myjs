9ac1d51769d68d201c5e90d2aa76b447571ebd12
brucewang1987 <30851957+brucewang1987@users.noreply.github.com>
2018-02-07 11:00:26 +0800

    Update RFjianhuJScript2_dwsb.js

--- RFjianhuJScript2_dwsb.js
+++ RFjianhuJScript2_dwsb.js
@@ -338,6 +338,22 @@
                     i++;
 
                     $("#DateIn").val(_json[key].INTO_PORT);
+                   
+                   
+                   var myDate = new Date();
+//获取当前年
+var year=myDate.getFullYear();
+//获取当前月
+var month=myDate.getMonth()+1;
+//获取当前日
+var date=myDate.getDate(); 
+var h=myDate.getHours();       //获取当前小时数(0-23)
+var m=myDate.getMinutes();     //获取当前分钟数(0-59)
+var s=myDate.getSeconds();  
+
+var now=year+'-'+p(month)+"-"+p(date)+" "+p(h)+':'+p(m);
+                   
+                   $("#ptiDateID").val(now);
 
 
                 });
