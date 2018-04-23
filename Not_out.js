993cd4eeabd53a984a674df1d3703ac2778598c9
brucewang1987 <30851957+brucewang1987@users.noreply.github.com>
2018-04-23 09:57:35 +0800

    Update Not_out.js

--- Not_out.js
+++ Not_out.js
@@ -1,63 +1,63 @@
- function QueryNotOut() {
+  $("#btnQuery").click(function () {
+
+                  $.ajax({
+
+                      type: "get",
+                      dataType: "json",
+                      url: "RFjianhuHandler.ashx?val8=" + "notOutList",
+
+                      success: function (ret) {
+
+                          var str = JSON.stringify(ret.dtNotOutList);
+                          if (str == "[]") {
+                              alert("没有数据");
+                              return false;
+                          }
 
 
-                $.ajax({
+                          var _json = eval(ret.dtNotOutList);
 
-                    type: "get",
-                    dataType: "json",
-                    url: "RFjianhuHandler.ashx?val8=" + "notOutList",
+                          var html = "";
+                          html += "<tr>";
 
-                    success: function (ret) {
+                          html += "<td>箱号</td>";
+                          html += "<td>是否记录</td>"
 
-                        var str = JSON.stringify(ret.dtNotOutList);
-                        if (str == "[]") {
-                            alert("没有数据");
-                            return false;
-                        }
+                          html += "</tr>"
 
 
-                        var _json = eval(ret.dtNotOutList);
+                          $(_json).each(function (key) {
 
-                        var html = "";
-                        html += "<tr>";
-
-                        html += "<td>箱号</td>";
-                        html+="<td>是否记录</td>"
-                      
-                        html += "</tr>"
+                              html += "<tr>";
+                              html += "<td>" + _json[key].ctn_no + "</td>";
+                              html += "<td>" + _json[key].is_record + "</td>";
+                              html += "</tr>";
 
 
-                        $(_json).each(function (key) {
+                          });
 
-                            html += "<tr>";
-                            html += "<td>" + _json[key].ctn_no + "</td>";
-                            html += "<td>" + _json[key].is_record + "</td>";
-                            html += "</tr>";
+                          $("#notouttable").html(html);
 
-
-                        });
-
-                        $("#notouttable").html(html);
-
-                    },
-                    error: function (XmlHttpRequest, textStatus, errorThrown) {
-                        alert(XmlHttpRequest.responseText);
-                    }
+                      },
+                      error: function (XmlHttpRequest, textStatus, errorThrown) {
+                          alert(XmlHttpRequest.responseText);
+                      }
 
 
-                })
+                  })
+
+              });
 
 
-            }
+              $("#btnTableToExcel").click(function () {
 
+                  $("#notouttable").table2excel({
+                      exclude: ".noExl",
+                      name: "Excel Document Name",
+                      filename: "myFileName",
+                      exclude_img: true,
+                      exclude_links: true,
+                      exclude_inputs: true
+                  });
 
-            function exportexcel() {
-                $("#notouttable").table2excel({
-                    exclude: ".noExl",
-                    name: "Excel Document Name",
-                    filename: "myFileName",
-                    exclude_img: true,
-                    exclude_links: true,
-                    exclude_inputs: true
-                });
-            }
+              });
