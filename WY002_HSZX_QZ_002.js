110c5089e84cd19cfcec6ee1cc473582c6f7fb8b
brucewang1987 <30851957+brucewang1987@users.noreply.github.com>
2018-08-22 10:07:54 +0800

    Update WY002_HSZX_QZ_002.js

--- WY002_HSZX_QZ_002.js
+++ WY002_HSZX_QZ_002.js
@@ -627,3 +627,216 @@
 
 
 
+ function divdisappear() {
+
+          $("#div2").hide();
+
+      }
+
+      divdisappear();
+
+
+      function divdisplay() {
+
+
+          $("#div2").show();
+
+      }
+
+
+      $("#btnTZSettlePL").click(function () {
+
+          if ($("table tr").length > 1) {
+
+
+              if (!window.confirm('确定要对这部分数据做退租，一旦做了退租就不能做租金结算操作')) {
+
+                  return false;
+              }
+
+
+              divdisplay();
+
+          }
+          else {
+
+              alert("请先查询起租数据");
+              return false;
+
+          }
+
+
+      });
+
+
+      $("#btnCancel").click(function () {
+
+
+          divdisappear();
+
+      });
+
+
+      $("#btnTZAdd").click(function () {
+
+     
+              TZInfoPL();
+         
+
+
+      });
+
+      function TZInfoPL() {
+
+
+         var startDate = $("#QZStartDate").val();
+
+         var endDate = $("#QZEndDate").val();
+
+
+          var regu = /^[0-9]+\.?[0-9]*$/;
+
+          var TZDate = $("#TZDate").val();
+
+          if (TZDate == "") {
+
+              alert("请输入退租日期");
+              return false;
+          
+          }
+
+          var r = TZDate.match(/^(\d{1,4})(-|\/)(\d{1,2})\2(\d{1,2})$/);
+          if (r == null) {
+              alert("请输入格式正确的日期\n\r日期格式：yyyy-mm-dd例    如：2008-08-08");
+              return false;
+          }
+
+          var TZNO = $("#TZNo").val();
+
+          if (TZNO == "") {
+
+              alert("请输入退租号");
+              return false;
+          
+          }
+
+          var TZAdress = $("#TZAdress").val();
+
+          var TZLoadFeeCoin = $("#TZLoadFeeCoin").find("option:selected").text();
+
+          var TZLoadFee = $("#TZLoadFee").val();
+
+          if (TZLoadFee == "") {
+
+              $("#TZLoadFee").val(0.00);
+
+          }
+
+          if (!regu.test(TZLoadFee)) {
+              alert("退租装卸费格式不正确");
+              $("#TZLoadFee").focus();
+              return false;
+
+          }
+
+
+          var TZPtiFeeCoin = $("#TZPtiFeeCoin").find("option:selected").text();
+
+          var TZPtiFee = $("#TZPtiFee").val();
+
+          if (TZPtiFee == "") {
+
+              $("#TZPtiFee").val(0.00);
+
+          }
+          if (!regu.test(TZPtiFee)) {
+              alert("退租pti费格式不正确");
+              $("#TZPtiFee").focus();
+              return false;
+
+          }
+
+
+          var TZTranFeeCoin = $("#TZTranFeeCoin").find("option:selected").text();
+
+          var TZTranFee = $("#TZTranFee").val();
+
+          if (TZTranFee == "") {
+
+              $("#TZTranFee").val(0.00);
+
+          }
+          if (!regu.test(TZTranFee)) {
+              alert("退租运输费格式不正确");
+              $("#TZTranFee").focus();
+              return false;
+
+          }
+
+          var TZXTFeeCoin = $("#TZXTFeeCoin").find("option:selected").text();
+
+          var TZXTFee = $("#TZXTFee").val();
+
+
+          if (TZXTFee == "") {
+
+              $("#TZXTFee").val(0.00);
+
+          }
+          if (!regu.test(TZXTFee)) {
+              alert("退租箱体修理费格式不正确");
+              $("#TZXTFee").focus();
+              return false;
+
+          }
+
+
+
+          var TZJZFeeCoin = $("#TZJZFeeCoin").find("option:selected").text();
+
+          var TZJZFee = $("#TZJZFee").val();
+
+
+          if (TZJZFee == "") {
+
+              $("#TZJZFee").val(0.00);
+
+          }
+          if (!regu.test(TZJZFee)) {
+              alert("退租机组修理费格式不正确");
+              $("#TZJZFee").focus();
+              return false;
+
+          }
+
+
+          $.ajax({
+
+              type: "get",
+              dataType: "json",
+              url: "/TZInfo/TZBusiness?val1=" + TZDate + "&val2=" + TZNO + "&val3=" + TZAdress
+              + "&val4=" + TZLoadFeeCoin + "&val5=" + TZLoadFee + "&val6=" + TZPtiFeeCoin+
+              "&val7=" + TZPtiFee + "&val8=" + TZTranFeeCoin + "&val9=" + TZTranFee+
+              "&val10=" + TZXTFeeCoin + "&val11=" + TZXTFee + "&val12=" + TZJZFeeCoin+
+              "&val13=" + TZJZFee + "&val14=" + startDate + "&val15=" + endDate + "&val88=" + "TZinfoAddPL",
+
+              success: function (ret) {
+
+                  var map = eval('(' + ret + ')');
+                  var response = JSON.stringify(map.Response);
+
+                  alert(response);
+
+                  window.location.reload();
+
+              }
+          
+                  
+          })     
+      
+      }
+
+
+
+
+
