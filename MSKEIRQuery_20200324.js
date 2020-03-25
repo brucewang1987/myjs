fe703e7b380e481e3de83da52fbd7afd4228eaed
brucewang1987 <30851957+brucewang1987@users.noreply.github.com>
2020-03-25 09:03:24 +0800

    Update MSKEIRQuery_20200324.js

--- MSKEIRQuery_20200324.js
+++ MSKEIRQuery_20200324.js
@@ -1,72 +1,80 @@
  $("#btnQuery").click(function () {
 
-            var startdate = $("#startdate").val();
+          var startdate = $("#startdate").val();
 
-            if (startdate != "") {
-                if (startdate.indexOf("00:00:00") < 0) {
+          if (startdate != "") {
+              if (startdate.indexOf("00:00:00") < 0) {
 
-                    startdate = startdate + " 00:00:00";
-                    $("#startdate").val(startdate);
+                  startdate = startdate + " 00:00:00";
+                  $("#startdate").val(startdate);
 
-                }
-            }
-
-
-            var enddate = $("#enddate").val();
-
-            if (enddate != "") {
-                if (enddate.indexOf("23:59:59") < 0) {
-
-                    enddate = enddate + " 23:59:59";
-                    $("#enddate").val(enddate);
-
-                }
-            }
-
-            var loc = $("#Loc").val();
+              }
+          }
 
 
-            console.log("开始时间" + startdate + "------结束时间" + enddate + ">>>>>堆场号是" + loc);
+          var enddate = $("#enddate").val();
+
+          if (enddate != "") {
+              if (enddate.indexOf("23:59:59") < 0) {
+
+                  enddate = enddate + " 23:59:59";
+                  $("#enddate").val(enddate);
+
+              }
+          }
+
+          var loc = $("#Loc").val();
 
 
-
-            $.ajax({
-
-                type: "get",
-                dataType: "json",
-                url: "/MSKEIR/RequestEIR?val1=" + loc + "&val2=" + startdate + "&val3=" + enddate + "&val88=" + "QueryInv",
-
-                success: function (ret) {
-
-                    var map = eval('(' + ret + ')');
-
-                    console.log(JSON.stringify(map));
-
-                    var str = JSON.stringify(map.yardStorage);
+          console.log("开始时间" + startdate + "------结束时间" + enddate + ">>>>>堆场号是" + loc);
 
 
 
-                    $("#p_result").text(JSON.stringify(map));
+          $.ajax({
 
-                    if (str == "[]") {
-                        alert("无数据");
-                        //return false;
-                        window.location.reload();
-                    }
+              type: "get",
+              dataType: "json",
+              url: "/MSKEIR/RequestEIR?place_id=" + loc + "&startdate=" + startdate + "&enddate=" + enddate + "&val88=" + "QueryInv",
 
-                    var _json = eval(map.dt);
+              success: function (ret) {
 
+                  var map = eval('(' + ret + ')');
 
-                    //                    console.log(_json);
+                  console.log(JSON.stringify(map));
 
-                    var response = JSON.stringify(map.Response);
-
-                }
+                  var str = JSON.stringify(map);
 
 
 
+                  $("#p_result").text(JSON.stringify(str));
 
-            })
+                  if (str == "[]") {
+                      alert("无数据");
+                      //return false;
+                      window.location.reload();
+                  }
+
+                  var _json = eval(map.dt);
 
 
-        });
+                  //                    console.log(_json);
+
+                  var response = JSON.stringify(map.Response);
+
+              }
+
+
+
+
+          })
+
+
+      });
+
+
+      $("#btnSend").click(function () {
+
+          var Invinfo = $("#p_result").text();
+
+
+      });
