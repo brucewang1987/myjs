da968069249f0d2b8ab84d4cc0150fb0badc1962
brucewang1987 <30851957+brucewang1987@users.noreply.github.com>
2020-04-24 09:37:03 +0800

    Update BrucewangPdaMainInfo.js

--- BrucewangPdaMainInfo.js
+++ BrucewangPdaMainInfo.js
@@ -1,4 +1,4 @@
-  function init() {
+ function init() {
 
             $("#r_id").hide();
 
@@ -18,15 +18,15 @@
         function RepairInfoReset(i) {
 
 
-//            var r_isGetData = $("#RepairbillInfotable").find("tr").eq(i).find("td").eq(14).text();
+            //            var r_isGetData = $("#RepairbillInfotable").find("tr").eq(i).find("td").eq(14).text();
 
-//            if (r_isGetData == "未被读取") {
+            //            if (r_isGetData == "未被读取") {
 
-//                alert("数据未被读取，不能执行此操作");
+            //                alert("数据未被读取，不能执行此操作");
 
-//                return false;
-//            
-//            }
+            //                return false;
+            //            
+            //            }
 
 
             var r_id = $("#r_id").val();
@@ -49,14 +49,14 @@
                     var response = JSON.stringify(map.Response);
 
                     alert(response);
-                
-                
+
+
                 }
 
 
             })
 
-        
+
         }
 
         $("#btnReset").click(function () {
@@ -123,10 +123,18 @@
                     html += "<td>尺寸</td>"; //6
                     html += "<td>箱况</td>"; //7
                     html += "<td>造箱年份</td>"; //8
-                    html += "<td>验箱时间</td>"; //9
-                    html += "<td>验箱人</td>"; //10
-                    html += "<td>验箱备注</td>"; //11
-                    html += "<td>是否已被读取</td>"; //12
+                    html += "<td>机组品牌</td>"; //9
+                    html += "<td>机组型号</td>"; //10
+                    html += "<td>机组序列号</td>"; //11
+                    html += "<td>机组冷冻液</td>"; //12
+                    html += "<td>机组电压</td>"; //13
+                    html += "<td>pti动作</td>"; //14
+                    html += "<td>pti状态</td>"; //15
+                    html += "<td>机组日期</td>"; //16
+                    html += "<td>验箱时间</td>"; //17
+                    html += "<td>验箱人</td>"; //18
+                    html += "<td>验箱备注</td>"; //19
+                    html += "<td>是否已被读取</td>"; //20
                     html += "</tr>"
 
 
@@ -134,27 +142,35 @@
 
                         var _r_id = _json[key].r_id;
 
-                       
+
 
                         html += "<tr>";
                         html += "<td>" + "<input type = " + "'" + "button" + "' value = " + "'" + "查看验箱明细"
-                      + "' onclick = '" + "return  Settlement(" + i + ")" + "'" + "/>" + "</td>"; //0
-                        html += "<td>" + "<input type = " + "'" + "button" + "' value = " + "'" + "重置验箱信息"
-                      + "' onclick = '" + "return  RepairInfoReset(" + i + ")" + "'" + "/>" + "</td>"; //1
-                        html += "<td style = " + "'" + "display:none;" + "'" + ">" + _json[key].r_id + "</td>"//2
-                        html += "<td style = " + "'" + "display:none;" + "'" + ">" + _json[key].r_cntno + "</td>"//3
+                      + "' onclick = '" + "return  Settlement(" + i + ")" + "'" + "/>" + "</td>"; //
+//                        html += "<td>" + "<input type = " + "'" + "button" + "' value = " + "'" + "重置验箱信息"
+//                      + "' onclick = '" + "return  RepairInfoReset(" + i + ")" + "'" + "/>" + "</td>"; //1
+                        html += "<td style = " + "'" + "display:none;" + "'" + ">" + _json[key].r_id + "</td>"//0
+                        html += "<td style = " + "'" + "display:none;" + "'" + ">" + _json[key].r_cntno + "</td>"//1
                         //                    html += "<td>" + _json[key].r_cntno + "</td>"; //1
-                        html += "<td>" + _json[key].r_cntweight + "</td>"; //4
-                        html += "<td>" + _json[key].r_cnt_netweight + "</td>"; //5
-                        html += "<td>" + _json[key].r_cnt_grossweight + "</td>"; //6
-                        html += "<td>" + _json[key].r_cnttype + "</td>"; //7
-                        html += "<td>" + _json[key].r_cntsize + "</td>"; //8
-                        html += "<td>" + _json[key].r_av + "</td>"; //9
-                        html += "<td>" + _json[key].r_cntym + "</td>"; //10
-                        html += "<td>" + _json[key].r_date + "</td>"; //11
-                        html += "<td>" + _json[key].r_user + "</td>"; //12
-                        html += "<td>" + _json[key].inspection_Remark + "</td>"; //13
-                        html += "<td>" + _json[key].r_isGetData + "</td>"; //14
+                        html += "<td>" + _json[key].r_cntweight + "</td>"; //2
+                        html += "<td>" + _json[key].r_cnt_netweight + "</td>"; //3
+                        html += "<td>" + _json[key].r_cnt_grossweight + "</td>"; //4
+                        html += "<td>" + _json[key].r_cnttype + "</td>"; //5
+                        html += "<td>" + _json[key].r_cntsize + "</td>"; //6
+                        html += "<td>" + _json[key].r_av + "</td>"; //7
+                        html += "<td>" + _json[key].r_cntym + "</td>"; //8
+                        html += "<td>" + _json[key].MACH_TYPE + "</td>"; //9
+                        html += "<td>" + _json[key].MODEL_NUM + "</td>"; //10
+                        html += "<td>" + _json[key].UNIT_SERIAL_NUM + "</td>"; //11
+                        html += "<td>" + _json[key].SnowSeed + "</td>"; //12
+                        html += "<td>" + _json[key].unit_voltage + "</td>"; //13
+                        html += "<td>" + _json[key].pti_type + "</td>"; //14
+                        html += "<td>" + _json[key].pti_status + "</td>"; //15
+                        html += "<td>" + _json[key].unit_date + "</td>"; //16
+                        html += "<td>" + _json[key].r_date + "</td>"; //17
+                        html += "<td>" + _json[key].r_user + "</td>"; //18
+                        html += "<td>" + _json[key].inspection_Remark + "</td>"; //19
+                        html += "<td>" + _json[key].r_isGetData + "</td>"; //20
                         html += "</tr>";
                         i++;
                     });
